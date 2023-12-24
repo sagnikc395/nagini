a6a97b3281e5a2639b69b6ac2ecc581101922dd6
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-25 00:37:36 +0530

    done for the day

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const ROWS = 30;
 const COLS = 50;
-const PIXELS = 10;
+const PIXEL = 10;
 
 const moveRight = ([t, l]) => [t, l + 1];
 const moveLeft = ([t, l]) => [t, l - 1];
@@ -52,8 +52,57 @@ const checkValidHead = (keys, cell) => {
 
 function App() {
   const [pixels, setPixels] = useState(new Map());
+  const [currentFoodKey, setCurrentFoodKey] = useState("");
+  const [currentDirection, setCurrentDirection] = useState(() => moveRight);
+  const [directionQueue, setDirectionQueue] = useState([]);
+  const [gameInterval, setGameInterval] = useState(null);
+  const [currentSnake, setCurrentSnake] = useState(initialSnakePosition());
+  const [currentSnakeKeys, setCurrentSnakeKeys] = useState(new Set());
+  const [currentVacantKeys, setCurrentVacantKeys] = useState(new Set());
 
-  return <></>;
+  const initializeCanvas = () => {
+    //load the canvas component
+    const pixelsMap = new Map();
+    for (let i = 0; i < ROWS; i++) {
+      for (let j = 0; j < COLS; j++) {
+        const key = toKey([i, j]);
+        pixelsMap.set(key, {
+          top: i * PIXEL,
+          left: j * PIXEL,
+          background: "white",
+        });
+      }
+    }
+    setPixels(pixelsMap);
+  };
+
+  return (
+    <div>
+      <div
+        id="canvas"
+        style={{
+          position: "relative",
+          width: `${COLS * PIXEL}px`,
+          height: `${ROWS * PIXEL}px`,
+        }}
+      >
+        {Array.from(pixels.values()).map(({ top, left, background }, index) => (
+          <div
+            key={index}
+            style={{
+              position: "absolute",
+              border: "1px solid #aaa",
+              top: `${top}px`,
+              left: `${left}px`,
+              width: `${PIXEL}px`,
+              height: `${PIXEL}px`,
+              background,
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
