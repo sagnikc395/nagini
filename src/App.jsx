01c6ab4ba10f090466584a8e08a4a95149daec63
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-25 00:16:54 +0530

    src: basic components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const ROWS = 30;
+const COLS = 50;
+const PIXELS = 10;
+
+const moveRight = ([t, l]) => [t, l + 1];
+const moveLeft = ([t, l]) => [t, l - 1];
+const moveUp = ([t, l]) => [t - 1, l];
+const moveDown = ([t, l]) => [t + 1, l];
+
+//game state logic
+const areOpposite = (dir1, dir2) => {
+  if (dir1 == moveLeft && dir2 == moveRight) {
+    return true;
+  }
+  if (dir1 === moveRight && dir2 === moveLeft) {
+    return true;
+  }
+  if (dir1 === moveUp && dir2 === moveDown) {
+    return true;
+  }
+  if (dir1 === moveDown && dir2 === moveUp) {
+    return true;
+  }
+  return false;
+};
+
+const initialSnakePosition = () => {
+  return [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ];
+};
+
+const checkValidHead = (keys, cell) => {
+  let [top, left] = cell;
+  if (top < 0 || left < 0) {
+    return false;
+  }
+  if (top >= ROWS || left >= COLS) {
+    return false;
+  }
+  if (keys.has(toKey(cell))) {
+    return false;
+  }
+  return true;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pixels, setPixels] = useState(new Map());
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
